2bbb8d5af7fe463b1f6f1816f1b0809eba35ed06
ayubova <ayubova.yuliya@gmail.com>
2017-08-03 18:25:52 +0300

    add practice

--- 01-Compound-Data/04-rational.js
+++ 01-Compound-Data/04-rational.js
@@ -1,19 +1,11 @@
-// eslint-disable-next-line
-import { cons, car, cdr, toString as pairToString } from 'hexlet-pairs';
+/import { cons, car, cdr, toString as pairToString } from 'hexlet-pairs';
 
 // BEGIN (write your solution here)
 export const make = (m,n) => cons(m,n);
 export const numer = rat => car(rat);
 export const denom = rat => cdr(rat);
 export const toString = (rat) => `${numer(rat)} / ${denom(rat)}`;
-export const isEqual = (rat1, rat2) => {
-  if (numer(rat1) / denom(rat1) === numer(rat2) / denom(rat2)) {
-    return true;
-  }
-  else {
-    return false;
-  }
-};
+export const isEqual = (rat1, rat2) => numer(rat1) / denom(rat1) === numer(rat2) / denom(rat2);
 export const add = (rat1, rat2) => {
   return make ((numer(rat1) * denom(rat2) + numer(rat2) * denom(rat1)), (denom(rat1) * denom(rat2)));
 };
