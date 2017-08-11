e189b82ab8bf2f958262a55b01483edc990b9a58
ayubova <ayubova.yuliya@gmail.com>
2017-08-11 16:51:28 +0300

    add practice

---
+++ Challenges/11-programming_basics_reexport.js
@@ -0,0 +1,9 @@
+/* eslint import/extensions: "off", import/no-unresolved: "off" */
+// BEGIN (write your solution here)
+import {pow2 as pow, sum2 as sum, sub2 as sub} from './numbers2';
+import cube from './numbers1';
+import  multi from './numbers2';
+import sqrt from './numbers3';
+export default cube;
+export {sqrt, multi, pow, sum, sub};
+// END
