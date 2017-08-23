9a62d7b4885a5f1e49dbc8bea37c0483d99959d7
ayubova <ayubova.yuliya@gmail.com>
2017-08-23 11:17:46 +0300

    add practice

--- Challenges/05-programming_basics_power3.js
+++ Challenges/05-programming_basics_power3.js
@@ -1,12 +1,9 @@
+// BEGIN (write your solution here)
 const isPowerOfThree = (num) => {
-
   for (let i = 1; num % 3 === 0; i++) {
-      num = num / 3;
-  }
-  if (num === 1) {
-    return true;
-  }
-      return false;
-      };
-
+    num = num / 3;
+      }
+      return num === 1;
+};
 export default isPowerOfThree;
+// END
